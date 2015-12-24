186bedc6a543e2723088a0f364164defbe4e4bc1
Pedro Howat <pedro@instant.com.br>
2015-12-23 22:43:00 -0200

    Initial Commit

---
+++ LibertAÇÃO/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LibertAÇÃO
+//
+//  Created by Pedro Rodrigues on 12/23/15.
+//  Copyright © 2015 Pedro Howat. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
